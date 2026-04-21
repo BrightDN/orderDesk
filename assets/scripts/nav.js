78a4d2070c4c7bfd5d08f69680c41fa4cec825b4
brightDN <brightondeneef@gmail.com>
2026-04-21 23:51:29 +0200

    Added assets for the current components

---
+++ assets/scripts/nav.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', () => {
+      document.querySelectorAll('.nav-item').forEach(n => n.classList.remove('active'));
+      item.classList.add('active');
+    });
+  });
