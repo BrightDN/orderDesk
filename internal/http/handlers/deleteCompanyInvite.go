c6dace9fb078fd35200df0e1a6758a42f91ae297
brightDN <brightondeneef@gmail.com>
2026-06-17 22:42:54 +0200

    updated logging

--- internal/http/handlers/deleteCompanyInvite.go
+++ internal/http/handlers/deleteCompanyInvite.go
@@ -1,7 +1,8 @@
 package handlers
 
 import (
-	"github.com/brightDN/orderDesk/internal/flash"
+	"github.com/brightDN/orderDesk/internal/shared/errorHandling"
+	"github.com/brightDN/orderDesk/internal/shared/logging"
 	"github.com/brightDN/orderDesk/internal/shared/parse"
 	"github.com/labstack/echo/v4"
 )
@@ -9,19 +10,19 @@ import (
 func (h *Handler) deleteCompanyInvite(c echo.Context) error {
 	id, err := parse.Int32(c.Param("id"))
 	if err != nil {
-		if flashErr := flash.Trigger(c, flash.Error, err.Error()); flashErr != nil {
-			return flashErr
+		if logErr := errorHandling.Log_and_flash(c, *err); logErr != nil {
+			return logErr
 		}
 		return h.renderInviteListPartial(c)
 	}
 	if err := h.App.Services.Invitations.Delete(c, id); err != nil {
-		if err := flash.Set(c, flash.Error, ErrInternalError.Error()); err != nil {
-			return err
+		if logErr := errorHandling.Log_and_flash(c, *err); logErr != nil {
+			return logErr
 		}
 		return h.renderInviteListPartial(c)
 	}
 
-	if err := flash.Set(c, flash.Pass, "Company invite successfully deleted."); err != nil {
+	if err := logging.Log_info_and_flash(c, "Deleted company invitation", "Company invite successfully deleted."); err != nil {
 		return err
 	}
 	return h.renderInviteListPartial(c)
